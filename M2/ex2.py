c88d82cdb65861568d5ffeaf4bd52f9cb4f66ef4
maducr <crmariaeduarda2003@gmail.com>
2023-12-29 09:16:19 -0400

    Exercises - Module 2

--- M2/ex2.py
+++
@@ -1,8 +0,0 @@
-# EXERCÍCIOS M2 - TRABALHANDO COM SISTEMAS DE COORDENADAS #
-# E1 - Crie um polígono a partir de uma lista de coordenadas
-# DICAS: Para acessar o arquivo você precisa montar o Google drive (como fizemos nas aulas), ou carregar do seu computador para o Colab.
-
-# a) Crie um polígono a partir das coordenadas x e y dadas no arquivo coords.txt.O sistema de coordenadas desses dados é o EPSG 29183.
-# b) Insira o polígono criado em um geodataframe
-# c) Salve o polígono em um shapefile
-# d) Plote e salve uma figura do polígono
